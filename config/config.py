2a3e701ea5c71a0ab3054b829262ff49b43dd85e
geekscruff <julie.allinson@york.ac.uk>
2014-03-27 19:45:16 +0000

    some more

---
+++ config/config.py
@@ -0,0 +1,11 @@
+__author__ = 'geekscruff'
+
+#Production configuration settings
+DEBUG = False
+TESTING = False
+SECRET_KEY = 'changeme'
+DATABASE = '/opt/peoplesparql.db'
+USERNAME = 'changeme'
+PASSWORD = 'changeme'
+
+
